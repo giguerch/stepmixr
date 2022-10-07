4ed6947c9ec589e23a7d0c227a68367d56fb1f16
giguerch <ce.giguere@gmail.com>
2022-10-06 21:28:54 -0400

    added list names in stepmix

--- R/stepmix.R
+++ R/stepmix.R
@@ -11,17 +11,20 @@ stepmix <- function(n_components = 2, n_steps = 1,
                     verbose = 0, verbose_interval = 10,
                     measurement_params = NULL, structural_params = NULL){
 
-
   sm_object = list(n_components = as.integer(n_components),
                    n_steps = as.integer(n_steps),
-                   measurement, structural, assignment,
-                   correction, abs_tol, rel_tol,
+                   measurement = measurement, 
+                   structural = structural, 
+                   assignment = assignment,
+                   correction = correction, 
+                   abs_tol = abs_tol, rel_tol = rel_tol,
                    max_iter = as.integer(max_iter),
                    n_init = as.integer(n_init),
-                   init_params, random_state,
+                   init_params = init_params, random_state = random_state,
                    verbose = as.integer(verbose),
                    verbose_interval = as.integer(verbose_interval),
-                   measurement_params, structural_params)
+                   measurement_params = measurement_params, 
+                   structural_params = structural_params)
   class(sm_object) <- "stepmixr"
   sm_object
 }
@@ -42,7 +45,8 @@ print.stepmixr <- function(x, ..., options = 1){
     }
   }
   else {
-    print(x)
+    cat("StepMix()\n")
+    print(unlist(x))
   }
 }
 
