cfc2f3512dc3aeb050078e12b0cb4c5b7add5ba3
giguerch <ce.giguere@gmail.com>
2022-10-13 21:24:05 -0400

    Added fit

--- R/stepmix.R
+++ R/stepmix.R
@@ -1,10 +1,10 @@
 ### model to use the stepmix class.
 
 ### Function stepmix to create a stepmix object. Kept as an R object. The
-### python package is used when we fit some data to it.
+### python package is only used when we fit some data to it.
 
 stepmix <- function(n_components = 2, n_steps = 1,
-                    measurement = "bernouilli", structural = "bernouilli",
+                    measurement = "bernoulli", structural = "bernoulli",
                     assignment = "modal", correction = NULL,
                     abs_tol = 1e-3, rel_tol = 1e-10, max_iter = 100,
                     n_init = 1, init_params = "random", random_state = NULL,
@@ -52,4 +52,52 @@ print.stepmixr <- function(x, ..., options = 1){
 
 ### fit stepmixr
 
+fit <- function(smx, X = NULL, Y = NULL){
+  ## if both x and y are null, we return smx
+  if(is.null(X) & is.null(Y)){
+    stop("Both X and Y aren't specified")
+  }
+  
+  if(is.null(X)){
+    stop("X must be specified")
+  }
+  # On fit X seulement.
+  if(is.null(Y)){
+    py_config <- try(reticulate::py_discover_config(required_module = "stepmix"))
+    # load
+    sm <- try(reticulate::import("stepmix"), silent = TRUE)
+    if(inherits(stepmix,"try-error"))
+          stop(paste("Unable to find stepmix library in your python repos\n",
+                     "Install it using pip install stepmix.",collapse = ""))
+    model <- do.call(sm$StepMix, smx)
+    fit <- model$fit(as.data.frame(X))
+    return(fit)
+  }
+  else{
+    py_config <- try(reticulate::py_discover_config(required_module = "stepmix"))
+    # load
+    sm <- try(reticulate::import("stepmix"), silent = TRUE)
+    if(inherits(stepmix,"try-error"))
+      stop(paste("Unable to find stepmix library in your python repos\n",
+                 "Install it using pip install stepmix.",collapse = ""))
+           model <- do.call(sm$StepMix, smx)
+           fit <- model$fit(as.data.frame(X), as.data.frame(Y))
+           return(fit)
+  }
+}
+
+
+require(reticulate)
+use_python("C:/Python39/")
+mod = stepmix()
+X = data.frame(x1 = c(0,1,0,1,1,0,0,0,1,1,1,0,0,1,0,1,1,0,0,1),
+               x2 = c(0,1,0,1,1,0,0,0,1,0,1,1,1,1,0,1,0,0,0,1))
+
+fit1 = fit(mod, X)
+f1 = file("fit1.pickle", "wb")
+reticulate::py_save_object(fit1, "fit1.pickle")
+close(f1)
+
+fit1 = reticulate::py_load_object("fit1.pickle") 
+fit1.pre
 ### predict stepmixr.fit
